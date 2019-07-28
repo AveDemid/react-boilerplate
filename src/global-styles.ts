33ee3108b7b107d4ad80da34a0b683580dcaf9f3
Demid Borodin <ave.demid@gmail.com>
2019-07-28 20:03:30 +0300

    Installed styled-reboot

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from "styled-components";
+import reboot from "styled-reboot";
+
+const rebootCss = reboot({});
 
 export const GlobalStyles = createGlobalStyle`
+  ${rebootCss}
+  
   *,
   *::before,
   *::after {
