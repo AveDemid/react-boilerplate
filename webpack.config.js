008a865009d667a76103609ce4bca31420a415a3
Demid Borodin <ave.demid@gmail.com>
2019-07-28 17:21:30 +0300

    Installed react, react-dom, @types/react, @types/react-dom, html-webpack-plugin, ts-loader, typescript, webpack, webpack-cli, webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle_[hash].min.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
